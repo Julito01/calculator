055ececa8321e1198bb7286d0f17258a65535bd7
Julito01 <julitod55@hotmail.com>
2022-02-17 00:38:45 -0300

    Finished the display of the numbers and the operations function

--- index.js
+++ index.js
@@ -7,30 +7,113 @@ const arrayOperators = document.querySelectorAll('[data-operator-button');
 const equalButton = document.querySelector('[data-equal-button');
 const operatorButtonArray = document.querySelectorAll('[data-operator-button]');
 
-// Display the numbers clicked
-Array.from(numberList).forEach((number) => {
-  number.addEventListener('click', (e) => {
-    if (currentNumberDisplay === '0' && number.innerText === '0') return;
-    if (currentNumberDisplay.innerText === '0') {
-      currentNumberDisplay.innerText = '';
+// This function is to remove the last character from the previous number
+function removeLastCharacter() {
+  let originalPreviousNumber = previousNumberDisplay.innerText.toString(); // This number has the operator as the last character
+  let previousNumber = originalPreviousNumber.slice(
+    0,
+    originalPreviousNumber.length - 1
+  );
+  return previousNumber; // Here i remove the last character, which is the operator
+}
+
+// Display the previous operand in another div
+let operatorClicked = '';
+let firstOperation = true;
+let operationCounter = 0;
+
+Array.from(operatorButtonArray).forEach((operatorButton) => {
+  operatorButton.addEventListener('click', (e) => {
+    if (currentNumberDisplay.innerText === '0') return;
+    if (firstOperation && operationCounter == 0) {
+      operatorClicked = operatorButton.innerText.toString();
+      previousNumberDisplay.innerText =
+        currentNumberDisplay.innerText + operatorButton.innerText;
+      currentNumberDisplay.innerText = '0';
+      firstOperation = false;
+      operationCounter += 1;
+    } else if (!firstOperation) {
+      let previousNumber = removeLastCharacter();
+      let operationResult = calculateOperation(
+        currentNumberDisplay.innerText,
+        previousNumber,
+        operatorClicked
+      );
+      operatorClicked = operatorButton.innerText;
+      operationCounter += 1;
+      currentNumberDisplay.innerText = operationResult;
+      previousNumberDisplay.innerText = operationResult + operatorClicked;
     }
-    currentNumberDisplay.innerText =
-      currentNumberDisplay.innerText.toString() + e.target.innerText.toString();
     e.preventDefault();
   });
 });
 
-// Display the previous operand in another div
-Array.from(operatorButtonArray).forEach((operatorButton) => {
-  operatorButton.addEventListener('click', (e) => {
-    console.log(operatorButton.innerText);
+// Display the numbers clicked
+Array.from(numberList).forEach((number) => {
+  number.addEventListener('click', (e) => {
+    let number = e.target.innerText.toString();
+    if (currentNumberDisplay.innerText === '0' && number.innerText === '0')
+      return;
+    if (currentNumberDisplay.innerText === '0') {
+      currentNumberDisplay.innerText = '';
+    }
+    if (operationCounter > 1 && !firstOperation) {
+      currentNumberDisplay.innerText = '';
+      operationCounter -= 1;
+    }
+    currentNumberDisplay.innerText += number;
     e.preventDefault();
   });
 });
+
+// Make the calc function and the equal button logic
+equalButton.addEventListener('click', (e) => {
+  // console.log(currentNumberDisplay.innerText);
+  // console.log(previousNumberDisplay.innerText);
+  let operatorClicked = previousNumberDisplay.innerText.toString().substr(-1);
+  let previousNumber = removeLastCharacter();
+  if (currentNumberDisplay.innerText === '0') {
+    currentNumberDisplay.innerText = 'Error.';
+    previousNumberDisplay.innerText = 'Cannot divide by 0';
+    return;
+  }
+  let operationResult = calculateOperation(
+    currentNumberDisplay.innerText,
+    previousNumber,
+    operatorClicked
+  );
+  currentNumberDisplay.innerText = operationResult;
+  previousNumberDisplay.innerText = '';
+  firstOperation = true;
+  operationCounter = 0;
+});
+
+function calculateOperation(n1, n2, operator) {
+  let operationResult = 0;
+  if (operator === '+') {
+    operationResult = parseInt(n1) + parseInt(n2);
+    return operationResult;
+  }
+  if (operator === '-') {
+    operationResult = parseInt(n2) - parseInt(n1);
+    return operationResult;
+  }
+  if (operator === '*') {
+    operationResult = parseInt(n1) * parseInt(n2);
+    return operationResult;
+  }
+  if (operator === '/') {
+    operationResult = parseInt(n1) / parseInt(n2);
+    return operationResult;
+  }
+}
 
 // Reset the value display
 resetButton.addEventListener('click', () => {
   currentNumberDisplay.innerText = 0;
+  previousNumberDisplay.innerText = '';
+  firstOperation = true;
+  operationCounter = 0;
   // console.log(numbersArray);
 });
 
@@ -42,6 +125,8 @@ backspaceButton.addEventListener('click', () => {
   currentNumberDisplay.innerText = numberUpdated;
   if (numberUpdated.length == 0) {
     currentNumberDisplay.innerText = '0';
+    operationCounter = 0;
+    firstOperation = true;
   }
 });
 
